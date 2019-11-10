d6436c49ade8a07fe03c8213e7eefccc71b76f0b
NithyaYamasinghe <nitheeromeshi@gmail.com>
2019-11-10 22:28:51 +0530

    third commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,7 +28,7 @@ class App extends React.Component {
 
     return (
       <div className="App">
-      <h1>Foody</h1>
+      <h1>FoodyZ</h1>
       <SearchBar searchYelp={this.searchYelp}/>
       <BusinessList businesses={this.state.businesses} />
     </div>
